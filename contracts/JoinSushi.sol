97afacd200b8a68c43236a5cf1a0f371afc9f5c1
Tomas Rawski <rtomas@gmail.com>
2023-11-08 16:30:03 +0100

    add NatSpec comments

--- contracts/JoinSushi.sol
+++ contracts/JoinSushi.sol
@@ -7,6 +7,8 @@ import "./SushiSwap/IMasterChefV2.sol";
 import "./UniSwap/IUniswapV2Factory.sol";
 import "./UniSwap/IUniswapV2Router01.sol";
 
+/// @title SushiSwapLiquidityInteract - A smart contract for interacting with SushiSwap for liquidity provision.
+/// @notice This contract allows the owner to provide liquidity for two ERC20 tokens on SushiSwap and deposit the LP tokens into MasterChef.
 contract SushiSwapLiquidityInteract {
     address public owner;
     IUniswapV2Router01 public sushiRouter;
@@ -28,7 +30,7 @@ contract SushiSwapLiquidityInteract {
         require(_tokenA != address(0), "Invalid Address");
         require(_tokenB != address(0), "Invalid Address");
         require(_tokenA != _tokenB, "Same Token Address");
-        
+
         owner = msg.sender;
         sushiRouter = IUniswapV2Router01(_sushiRouter);
         sushiFactory = IUniswapV2Factory(sushiRouter.factory());
@@ -43,18 +45,22 @@ contract SushiSwapLiquidityInteract {
         _;
     }
 
+    /// @notice Allows the owner to provide liquidity for two tokens on SushiSwap and deposit the LP tokens into MasterChef.
+    /// @param _amountA The amount of tokenA to provide for liquidity.
+    /// @param _amountB The amount of tokenB to provide for liquidity.
+    /// @param _pid The pool ID in MasterChef where the LP tokens will be deposited.
     function JoinLiquidity(uint256 _amountA, uint256 _amountB, uint _pid) public OnlyOwner {
         require(_amountA > 0, "Invalid TokenA Supply");
         require(_amountB > 0, "Invalid TokenB Supply");
-        require(CheckBalance(tokenA) >= _amountA ,"Insuficient Balance TokenA");
-        require(CheckBalance(tokenB) >= _amountB ,"Insuficient Balance TokenB");
+        require(CheckBalance(tokenA) >= _amountA, "Insuficient Balance TokenA");
+        require(CheckBalance(tokenB) >= _amountB, "Insuficient Balance TokenB");
 
         approveSushiRouterToUseTokens(_amountA, _amountB);
-        
+
         // check allowance
-        require(CheckAllowance(tokenA) >= _amountA ,"Insuficient Allowance A");
-        require(CheckAllowance(tokenB) >= _amountB ,"Insuficient Allowance B");
-        
+        require(CheckAllowance(tokenA) >= _amountA, "Insuficient Allowance A");
+        require(CheckAllowance(tokenB) >= _amountB, "Insuficient Allowance B");
+
         uint liquidity;
         (_amountA, _amountB, liquidity) = provideLiquidity(_amountA, _amountB);
 
@@ -65,17 +71,20 @@ contract SushiSwapLiquidityInteract {
         emit JoinLiquidityEvent(liquidity, _amountA, _amountB);
     }
 
-
-    // Approve SushiSwap Router to spend tokens
-    function approveSushiRouterToUseTokens(
-        uint256 _amountA,
-        uint256 _amountB
-    ) private OnlyOwner {
+    /// @notice Approves SushiSwap Router to spend tokens on behalf of the contract.
+    /// @param _amountA The amount of tokenA to approve for spending.
+    /// @param _amountB The amount of tokenB to approve for spending.
+    function approveSushiRouterToUseTokens(uint256 _amountA, uint256 _amountB) private OnlyOwner {
         IERC20(tokenA).approve(address(sushiRouter), _amountA);
         IERC20(tokenB).approve(address(sushiRouter), _amountB);
     }
 
-    // Provide liquidity on SushiSwap
+    /// @notice Provides liquidity on SushiSwap by swapping tokenA and tokenB for LP tokens.
+    /// @param amountA The amount of tokenA to provide for liquidity.
+    /// @param amountB The amount of tokenB to provide for liquidity.
+    /// @return pAmountA The actual amount of tokenA provided for liquidity.
+    /// @return pAmountB The actual amount of tokenB provided for liquidity.
+    /// @return liquidity The amount of LP tokens obtained from providing liquidity.
     function provideLiquidity(
         uint256 amountA,
         uint256 amountB
@@ -83,57 +92,61 @@ contract SushiSwapLiquidityInteract {
         uint256 valueA = sushiRouter.quote(amountB, amountB, amountA);
         uint256 valueB = sushiRouter.quote(amountA, amountA, amountB);
 
-        (uint pAmountA, uint pAmountB, uint liquidity) =
-            sushiRouter.addLiquidity(
-                address(tokenA),
-                address(tokenB),
-                valueA,
-                valueB,
-                1, 
-                1,
-                address(this),
-                block.timestamp + 1000 * 60 * 5   
-            );
+        (uint pAmountA, uint pAmountB, uint liquidity) = sushiRouter.addLiquidity(
+            address(tokenA),
+            address(tokenB),
+            valueA,
+            valueB,
+            1,
+            1,
+            address(this),
+            block.timestamp + 1000 * 60 * 5
+        );
         return (pAmountA, pAmountB, liquidity);
     }
 
-    // Approve MasterChef to spend LP tokens
-    function approveMasterChef( uint256 amountLP) private OnlyOwner {
+    /// @notice Approves MasterChef to spend LP tokens on behalf of the contract.
+    /// @param amountLP The amount of LP tokens to approve for spending.
+    function approveMasterChef(uint256 amountLP) private OnlyOwner {
         address addressPoolAB = getPoolPairAddressForTokens();
         IERC20(addressPoolAB).approve(address(masterChef), amountLP);
     }
 
-    //  Deposit LP tokens into MasterChef
+    /// @notice Deposits LP tokens into MasterChef based on the specified pool ID.
+    /// @param _pid The pool ID in MasterChef where the LP tokens will be deposited.
+    /// @param liquidity The amount of LP tokens to deposit.
     function depositLP(uint _pid, uint256 liquidity) private OnlyOwner {
         if (isMasterChefV1) {
             IMasterChef(masterChef).deposit(_pid, liquidity);
         } else {
-            IMasterChefV2(masterChef).deposit(_pid, liquidity, address(this)); 
+            IMasterChefV2(masterChef).deposit(_pid, liquidity, address(this));
         }
     }
 
+    /// @notice Retrieves the address of the SushiSwap pair for the specified token pair (tokenA and tokenB).
+    /// @return The address of the SushiSwap pair for the specified token pair.
     function getPoolPairAddressForTokens() public view returns (address) {
-        return
-            IUniswapV2Factory(sushiFactory).getPair(
-                address(tokenA),
-                address(tokenB)
-            );
+        return IUniswapV2Factory(sushiFactory).getPair(address(tokenA), address(tokenB));
     }
 
-    // Owner can withdraw any remaining tokens from the contract
-    function withdrawTokens(
-        address tokenAddress,
-        uint256 amount
-    ) external OnlyOwner {
+    /// @notice Allows the owner to withdraw any remaining tokens from the contract.
+    /// @param tokenAddress The address of the token to be withdrawn.
+    /// @param amount The amount of tokens to be withdrawn.
+    function withdrawTokens(address tokenAddress, uint256 amount) external OnlyOwner {
         IERC20(tokenAddress).transfer(owner, amount);
     }
 
-    function CheckAllowance(IERC20 _Token) internal view returns(uint) {
+    /// @notice Checks the allowance of a specific token for the SushiSwap Router contract.
+    /// @param _Token The token for which to check the allowance.
+    /// @return The current allowance for the specified token.
+    function CheckAllowance(IERC20 _Token) internal view returns (uint) {
         return IERC20(_Token).allowance(address(this), address(sushiRouter));
     }
 
-    function CheckBalance(IERC20 _Token) internal view returns(uint) {
+    /// @notice Checks the balance of a specific token held by the contract.
+    /// @param _Token The token for which to check the balance.
+    /// @return The current balance of the specified token held by the contract.
+    function CheckBalance(IERC20 _Token) internal view returns (uint) {
         return IERC20(_Token).balanceOf(address(this));
     }
-    
 }
